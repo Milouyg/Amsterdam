712a319456b6618d0218648274e6202fc2080f2b
Milouyg <33333@ma-web.nl>
2023-12-06 20:28:21 +0100

    Added main and footer folder

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import footer from "../../../assets/json/info.json";
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent {
+
+    constructor(){
+
+    }
+
+}
