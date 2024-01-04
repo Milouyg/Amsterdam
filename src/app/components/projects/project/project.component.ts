904e79d17a2f24f80a7e61ba433fef011d98fe56
Milouyg <33333@ma-web.nl>
2024-01-04 18:19:35 +0100

    Made the project page dynamic with the title and paragraphs

--- src/app/components/projects/project/project.component.ts
+++ src/app/components/projects/project/project.component.ts
@@ -1,22 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import info from "../../../../assets/json/info.json";
 
 @Component({
 
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss']
 })
-export class ProjectComponent implements OnInit{
+export class ProjectComponent{
+
     title?:string;
+    projects = info['projects']['projects-content'];
+    paragraphs!:any;
+    projectInfo!:any;
 
     constructor( private route: ActivatedRoute ){
-
+        this.findProject();
     }
 
-    ngOnInit(){
-        const routing = this.route.snapshot.paramMap.get('title')
-        if( routing !== null){
-            this.title = routing;
-        }
+    findProject(){
+        this.projects.forEach(project => {
+            if(project['title'] === this.route.snapshot.paramMap.get('title')){
+                this.projectInfo = project;
+                this.paragraphs = project['paragraphs']
+            }
+        });
     }
 }
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -10,6 +10,6 @@ export class ProjectsComponent {
     projects = info['projects'];
 
     constructor(){
-
+    
     }
 }
